4d384507e3b87b65dfb17462635a1a746a28eded
Nurbakhyt <n_u_r_y_k@mail.ru>
2017-02-21 22:26:26 +0500

    2nd commit

--- src/app.jsx
+++ src/app.jsx
@@ -5,14 +5,12 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-// import { syncHistoryWithStore } from 'react-router-redux'
 import 'react-select/dist/react-select.css'
 
 import routes from './routes'
 import reducer from './reducers'
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
-// const history = syncHistoryWithStore(browserHistory, store)
 
 render(
   <Provider store={store}>
